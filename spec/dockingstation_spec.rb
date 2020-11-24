344b91dadc537880aee251318d18b715abdb491e
Natasha McIntyre <natashamc.cs@gmail.com>
2020-11-24 16:22:48 +0000

    added bike tests

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -5,4 +5,12 @@ describe DockingStation do
     it 'docking_station responds to release_bike' do
       expect(docking_station).to respond_to (:release_bike)
     end
+
+    describe '#release_bike' do
+      it 'gets a bike' do
+        expect(docking_station.release_bike).to eq bike
+      end
+    end
+
+
 end
