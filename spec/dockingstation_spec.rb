060cb5fc61cf4152f69f1618dc6500c02605549e
Natasha McIntyre <natashamc.cs@gmail.com>
2020-11-25 14:21:27 +0000

    boris day 1

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -2,7 +2,7 @@ require 'dockingstation'
 
 describe DockingStation do
   subject(:docking_station) { described_class.new }
-  let(:bike) { bike = double }
+  let(:bike) { Bike.new }
     it 'docking_station responds to release_bike' do
       expect(docking_station).to respond_to (:release_bike)
     end
@@ -12,10 +12,11 @@ describe DockingStation do
         expect(docking_station.release_bike).to be_an_instance_of(Bike)
       end
 
-      it 'checks bike is working' do
+      it 'checks bike working' do
         expect(bike.working?).to eq true
       end
     end
+
 
 
 end
